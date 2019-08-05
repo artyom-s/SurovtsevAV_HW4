5fb3f9e4002e72c4a87c1f136cc7ec8af32dd5dd
ArtyomSurovtsev <serovtsev.artyom@gmail.com>
2019-08-05 10:56:44 +0300

    Initial Commit

---
+++ SurovtsevAV_HW4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SurovtsevAV_HW4
+//
+//  Created by Артем on 05/08/2019.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
